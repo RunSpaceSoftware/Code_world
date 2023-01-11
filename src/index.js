0d07f4daf915433f2b9b204152274935c1ced03d
shayan <shayan.fr87@gmail.com>
2023-01-11 13:54:18 +0330

    cars

---
+++ src/index.js
@@ -0,0 +1,8 @@
+function hello() {
+    document.getElementById('jt').innerHTML = 'Hello';
+}
+
+function meth() {
+    const cars = ['206','red','100%']
+    document.getElementById('mo').innerHTML = cars;
+}
