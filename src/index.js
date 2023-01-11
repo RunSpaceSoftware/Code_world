784e5490b5687cdab10417dfea80d6dc3575d540
RunSpaceSoftware <shayanfr1387@gmail.com>
2023-01-11 22:17:10 +0330

    commented code

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
+//changes the html element 'jt'
 function hello() {
     document.getElementById('jt').innerHTML = 'Hello';
 }
+
+//this function gets 'cars' and when a button
+//is pressed execuits the resalt of the operation
+//and display's it on the screen
 
 function meth(cars) {
     cars = 6;
