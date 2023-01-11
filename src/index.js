f111889b890b240b6701f062158891af0e639392
RunSpaceSoftware <shayanfr1387@gmail.com>
2023-01-11 21:27:37 +0330

    function changes

--- src/index.js
+++ src/index.js
@@ -2,9 +2,8 @@ function hello() {
     document.getElementById('jt').innerHTML = 'Hello';
 }
 
-function meth() {
-    let cars = 6;
+function meth(cars) {
+    cars = 6;
     cars += 2;
-    cars++
     document.getElementById('mo').innerHTML = cars;
 }
